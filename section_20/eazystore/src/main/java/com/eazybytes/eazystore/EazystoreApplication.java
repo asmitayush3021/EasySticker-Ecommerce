de4686b97f11aecd7d4314df859014eb769c2f22
Asmit Kumar Singh <asmit.kumarsingh.cse26@heritageit.edu.in>
2026-03-27 00:18:46 +0530

    clean repo (no secrets)

---
+++ section_20/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableCaching
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+public class EazystoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazystoreApplication.class, args);
+    }
+
+}
